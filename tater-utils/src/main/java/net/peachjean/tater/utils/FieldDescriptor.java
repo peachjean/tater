723f0e8b88e9275cfd3dd5bd06a0dea6d94782f3
Jared Bunting <jared.bunting@peachjean.com>
2012-08-07 18:31:35 -0500

    Made array annotation value types varargs in annotation builder.

--- tater-utils/src/main/java/net/peachjean/tater/utils/FieldDescriptor.java
+++ tater-utils/src/main/java/net/peachjean/tater/utils/FieldDescriptor.java
@@ -3,12 +3,18 @@ package net.peachjean.tater.utils;
 class FieldDescriptor {
     private final String name;
     private final String type;
+    private final String parameterType;
     private final String defaultValue;
 
     FieldDescriptor(String name, String type, String defaultValue) {
         this.name = name;
         this.type = type;
         this.defaultValue = defaultValue;
+        if(type.endsWith("[]")) {
+            this.parameterType = type.substring(0, type.length() - 2) + " ...";
+        } else {
+            this.parameterType = type;
+        }
     }
 
     public String getName() {
@@ -17,6 +23,14 @@ class FieldDescriptor {
 
     public String getType() {
         return type;
+    }
+
+    public String getParameterType() {
+        if(type.endsWith("[]")) {
+            return type.substring(0, type.length() - 2) + " ...";
+        } else {
+            return type;
+        }
     }
 
     public String getDefaultValue() {
